c760dbacab720d9f5c55efeffbee63cb5b0b8193
JakobBB235 <33541242+JakobBB235@users.noreply.github.com>
2019-04-28 16:33:40 +0200

    Item service is now used to create

--- src/app/content/admin/products/create-or-edit-item/create-or-edit-item.component.ts
+++ src/app/content/admin/products/create-or-edit-item/create-or-edit-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
+import { ItemDataService } from 'src/app/services/item-data.service';
+import { Item } from 'src/app/entities/item';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-or-edit-item',
@@ -8,10 +11,10 @@ import { Validators, FormBuilder } from '@angular/forms';
 })
 export class CreateOrEditItemComponent implements OnInit {
 
-  isCreatingItem: boolean;
+  isCreatingItem: boolean = true; //Make logic to set this correctly
   productForm;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private router: Router, private itemDataService: ItemDataService) { }
 
   ngOnInit() {
     //Reactive form validation
@@ -30,9 +33,49 @@ export class CreateOrEditItemComponent implements OnInit {
         inStock: ['', Validators.required],
         price: ['', Validators.required],
         description: ['', Validators.required],
-        category: ['', Validators.required] //Enable when category class is created?
+        category: ['', Validators.required] //Create category model???
       }
     );
   }
 
+  onSubmit(productForm){
+    if (productForm.valid){
+      console.log("isCreatingItem" + this.isCreatingItem);
+      if(this.isCreatingItem) {
+        this.createNewProduct(productForm);
+      }
+      else if (!this.isCreatingItem) {
+        this.updateExistingProduct(productForm);
+      }
+    } 
+    else {
+      alert("Form is invalid!");
+    }
+    console.log(productForm) //Remove later
+  }
+  
+  createNewProduct(productForm){
+    let item = productForm.value as Item;
+    this.itemDataService.addItem(item).subscribe((response: Item) => {  
+      //If all goes well:
+      console.log(response); //Remove later
+      this.router.navigate(['/products/' + response.id]); //Navigate or stay?
+    }, error => {
+      //If web service fails:
+      console.log("Error!", error);
+    }); 
+  }
+
+  //Not in use yet.
+  updateExistingProduct(productForm){
+    let item = productForm.value as Item;
+    this.itemDataService.updateItem(item).subscribe((response: Item) => {  
+      //If all goes well:
+      console.log(response);
+      this.router.navigate(['/products/' + item.id]); //Navigate or stay?
+    }, error => {
+      //If web service fails:
+      console.log("Error!", error);
+    }); 
+  }
 }
